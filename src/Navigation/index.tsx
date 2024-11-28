7efdab8070c00d11ea3aa95c95ca54900408257c
Huy Lê Đình <huy.ledinh@hcmut.edu.vn>
2024-11-28 20:51:24 +0700

    reconfig navigattion

--- src/Navigation/index.tsx
+++ src/Navigation/index.tsx
@@ -3,12 +3,16 @@ import { StatusBar } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { MainNavigator } from "./Main";
-import { WelcomeContainer } from "@/Screens/Welcome";
-import { RootScreens } from "@/Screens";
+import { RootScreens, Onboarding1Container, Onboarding2Container, Onboarding3Container, Onboarding4Container, Onboarding5Container } from "@/Screens";
 
 export type RootStackParamList = {
   [RootScreens.MAIN]: undefined;
-  [RootScreens.WELCOME]: undefined;
+  [RootScreens.ONBOARDING1]: undefined;
+  [RootScreens.ONBOARDING2]: undefined;
+  [RootScreens.ONBOARDING3]: undefined;
+  [RootScreens.ONBOARDING4]: undefined;
+  [RootScreens.ONBOARDING5]: undefined;
+
 };
 
 const RootStack = createNativeStackNavigator<RootStackParamList>();
@@ -20,8 +24,24 @@ const ApplicationNavigator = () => {
       <StatusBar />
       <RootStack.Navigator screenOptions={{ headerShown: false }}>
         <RootStack.Screen
-          name={RootScreens.WELCOME}
-          component={WelcomeContainer}
+          name={RootScreens.ONBOARDING1}
+          component={Onboarding1Container}
+        />
+        <RootStack.Screen
+          name={RootScreens.ONBOARDING2}
+          component={Onboarding2Container}
+        />
+        <RootStack.Screen
+          name={RootScreens.ONBOARDING3}
+          component={Onboarding3Container}
+        />
+        <RootStack.Screen
+          name={RootScreens.ONBOARDING4}
+          component={Onboarding4Container}
+        />
+        <RootStack.Screen
+          name={RootScreens.ONBOARDING5}
+          component={Onboarding5Container}
         />
         <RootStack.Screen
           name={RootScreens.MAIN}
--- src/Screens/index.ts
+++ src/Screens/index.ts
@@ -1,4 +1,17 @@
+import { HomeContainer } from "./Home";
+import { Onboarding1Container } from "./Onboarding1";
+import { Onboarding2Container } from "./Onboarding2";
+import { Onboarding3Container } from "./Onboarding3";
+import { Onboarding4Container } from "./Onboarding4";
+import { Onboarding5Container } from "./Onboarding5";
+
 export enum RootScreens {
   MAIN = "Main",
-  WELCOME = "Welcome",
+  ONBOARDING1 = "Onboarding1",
+  ONBOARDING2 = "Onboarding2",
+  ONBOARDING3 = "Onboarding3",
+  ONBOARDING4 = "Onboarding4",
+  ONBOARDING5 = "Onboarding5",
 }
+
+export {HomeContainer, Onboarding1Container, Onboarding2Container, Onboarding3Container, Onboarding4Container, Onboarding5Container}
