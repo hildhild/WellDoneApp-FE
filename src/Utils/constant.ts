95efe789e36162778bba149675989dd1a70ff288
PhamThi1710 <92194681+PhamThi1710@users.noreply.github.com>
2024-12-01 14:29:41 +0700

    feat: Update password pattern to enforce stronger security requirements

--- src/Utils/constant.ts
+++ src/Utils/constant.ts
@@ -1,3 +1,11 @@
 export const EMAIL_PATTERN = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
 
-export const PASSWORD_PATTERN = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/; // Minimum 6 characters, at least one letter and one number
+export const PASSWORD_PATTERN =
+  /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{6,}$/; 
+// Password must contain 
+//   one digit from 1 to 9, 
+//   one lowercase letter, 
+//   one uppercase letter, 
+//   one special character, 
+//   no space, 
+//   and it must be at least 6 characters long
