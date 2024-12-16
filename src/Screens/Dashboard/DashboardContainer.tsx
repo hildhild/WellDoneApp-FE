c686b98c6f0e93fd83ef568c9f4edb822ba0422d
Huy Lê Đình <huy.ledinh@hcmut.edu.vn>
2024-12-16 17:03:55 +0700

    add dashboard and group main ui

--- src/Screens/Dashboard/DashboardContainer.tsx
+++ src/Screens/Dashboard/DashboardContainer.tsx
@@ -1,8 +1,15 @@
 import { Dashboard } from "./Dashboard";
 import React, { useState, useEffect } from "react";
 import { useLazyGetUserQuery } from "@/Services";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "@/Navigation";
+import { RootScreens } from "..";
 
-export const DashboardContainer = () => {
+type DashboardScreenNavigatorProps = NativeStackScreenProps<RootStackParamList>;
+export const DashboardContainer = ({ navigation }: DashboardScreenNavigatorProps) => {
+  const onNavigate = (screen: RootScreens) => {
+    navigation.navigate(screen);
+  };
   const [userId, setUserId] = useState("9");
 
   const [fetchOne, { data, isSuccess, isLoading, isFetching, error }] =
@@ -12,5 +19,5 @@ export const DashboardContainer = () => {
     fetchOne(userId);
   }, [fetchOne, userId]);
 
-  return <Dashboard data={data} isLoading={isLoading} />;
+  return <Dashboard onNavigate={onNavigate} data={data} isLoading={isLoading} />;
 };
--- src/Screens/Group/GroupContainer.tsx
+++ src/Screens/Group/GroupContainer.tsx
@@ -1,16 +1,24 @@
 import { Group } from "./Group";
 import React, { useState, useEffect } from "react";
 import { useLazyGetUserQuery } from "@/Services";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "@/Navigation";
+import { RootScreens } from "..";
+import { data } from "@/Utils/constant";
 
-export const GroupContainer = () => {
+
+type GroupScreenNavigatorProps = NativeStackScreenProps<RootStackParamList>;
+export const GroupContainer = ({ navigation }: GroupScreenNavigatorProps) => {
+  const onNavigate = (screen: RootScreens) => {
+    navigation.navigate(screen);
+  };
   const [userId, setUserId] = useState("9");
 
-  const [fetchOne, { data, isSuccess, isLoading, isFetching, error }] =
-    useLazyGetUserQuery();
-
+  const [fetchOne, { isSuccess, isLoading, isFetching, error }] = useLazyGetUserQuery();
+  
   useEffect(() => {
     fetchOne(userId);
   }, [fetchOne, userId]);
 
-  return <Group data={data} isLoading={isLoading} />;
+  return <Group onNavigate={onNavigate} data={data} isLoading={isLoading} />;
 };
