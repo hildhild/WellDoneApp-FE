f3c289342baac8d806ab8ee7eea02f957f2c458a
Huy Lê Đình <huy.ledinh@hcmut.edu.vn>
2024-12-22 00:16:33 +0700

    config sentry

--- App.tsx
+++ App.tsx
@@ -1,4 +1,10 @@
 import App from "./src";
 import "./global.css"
+import * as Sentry from "@sentry/react-native";
 
-export default App;
+Sentry.init({
+  dsn: "https://941c4ae12cccb242e29bc9c8b3f5a52b@o4508507008598016.ingest.us.sentry.io/4508507025702912",
+});
+
+export default Sentry.wrap(App);
+
